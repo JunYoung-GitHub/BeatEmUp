7a9dd9a9e9768a8d050bbb75d9785bad0342af0e
Jun <junyoungwon97@gmail.com>
2022-12-11 17:15:55 -0700

    Done up to shop sprite

--- JavaScriptPunchy/index.js
+++ JavaScriptPunchy/index.js
@@ -14,33 +14,52 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 //-------------------------------Setup proj-------------------------------//
 
 //-------------------------------Player/Enemy-------------------------------//
-//Create Sprite
-class Sprite {
-    constructor(position) {
-        this.position = position;
-    }
-    //Sprtite is drawn when method is called
-    draw() {
-        //Fill with red color
-        c.fillStyle = "red";
-        //50px wide and 150px tall at position location
-        c.fillRect(this.position.x, this.position.y, 50, 150);
-    }
-}
+//Gravity duh ` How fast the sprites fall
+const gravity = 0.7;
 
+//Position and image path of the background
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+})
 //Player 
 //Sprite arguments are position of the player sprite
-const player = new Sprite({
-    x:0,
-    y:0
+const player = new Fighter({
+   position: {
+        x:0,
+        y:0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    },
+    offset: {
+        x: 0,
+        y: 0
+    }
 })
 
 //Enemy
 //Same thing but it draws an enemy!
-const enemy = new Sprite ({
-    x: 400,
-    y: 100
-})
+const enemy = new Fighter({
+    position: {
+         x:400,
+         y:100
+     },
+     velocity: {
+         x: 0,
+         y: 0
+     },
+
+     offset: {
+        x: -50,
+        y: 0
+    },
+     color: 'blue'
+ })
 
 //Call draw method to draw player
 player.draw();
@@ -48,5 +67,219 @@ player.draw();
 //Call draw method to draw enemy
 enemy.draw();
 
-console.log(player)
-//-------------------------------Player/Enemy-------------------------------//
+console.log(player)//Debug to find location of the player!
+//Create objects for pressed keys
+const keys = {
+    a: {
+        pressed:false
+    },
+
+    d: {
+        pressed:false
+    },
+
+    ArrowRight: {
+        pressed:false
+    },
+
+    ArrowLeft: {
+        pressed:false
+    }
+}
+
+//Function for detecting collision of hitboxes to hurtboxes (attacking)
+function rectangularCollision({rectangle1, rectangle2}) {
+    return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= enemy.position.x
+        && rectangle1.attackBox.position.x <= rectangle2.position.x + enemy.width
+        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= enemy.position.y
+        && rectangle1.attackBox.position.y <= rectangle2.position.y + enemy.height)
+}
+
+//-------------------------------Game Timer/Game over-------------------------------//
+//Determines the winner based on whoever has the highest health left
+function determineWinner({player, enemy, timerId}) {
+    clearTimeout({timerId})
+    document.querySelector('#displayText').style.display = 'flex';
+    if(player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie';
+        console.log('tie');
+    }
+    //Display player 1 Win when player health is greater than the enemy health
+    else if(player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 wins';
+        console.log('Player Wins');
+    }
+    //Display player 1 Win when player health is greater than the enemy health
+    else if(player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 wins';
+        console.log('Enemy Wins');
+    }
+    //Error Message ############REMOVE LATER##############
+    else {
+        document.querySelector('#displayText').innerHTML = 'Error Something messed up';
+        console.log('Error Debug');
+    }
+}
+
+let timer = 60;
+//Time left in the timer maybe change name?
+let timerId
+function decreaseTimer() {
+    console.log("Timer Flag");
+    //recursively calls itself to reduce the timer every second (Maybe replace with setInterval???) ***************************************************
+    timerId = setTimeout(decreaseTimer, 1000)
+    if(timer > 0) {
+        timer--;
+        document.querySelector('#timer').innerHTML = timer;
+    }
+    //Display tie when both player's health is equal
+    if(timer === 0) {
+        determineWinner({player, enemy, timerId});
+    }
+}
+
+decreaseTimer();
+
+//Infinite loop animate
+function animate() {
+    //Loops this animation over and over again
+    window.requestAnimationFrame(animate);
+    //Make background black to differenciate sprite from background
+    c.fillStyle = 'black';
+    //Makes sure that the "past" sprites are clear to avoid "Painting" effect
+    c.fillRect(0, 0, canvas.width, canvas.height);
+    //draws background
+    background.update();
+    //Makes sure that the player sprite is called every update
+    player.update();
+    //Same thing with enemy
+    enemy.update();
+   
+    //Stops player and enemy momentum after horizontal key press
+    player.velocity.x = 0;
+    enemy.velocity.x = 0;
+
+    //Player movement Speed
+    if(keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = -5;
+    }
+    else if(keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = 5
+    }
+
+    //Enemy Movement Speed
+    enemy.velocity.x = 0;
+    if(keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -5;
+    }
+    else if(keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 5
+    }
+    //Detects collision player
+    if(rectangularCollision({rectangle1: player, rectangle2: enemy})
+        && player.isAttacking) {
+            player.isAttacking = false;
+            enemy.health -= 20;
+            document.querySelector('#enemyHealth').style.width=enemy.health + '%';
+            console.log("Player attack connects")
+    }
+    //detects collision enemy
+    if(rectangularCollision({rectangle1: enemy, rectangle2: player})
+        && enemy.isAttacking) {
+            enemy.isAttacking = false;
+            player.health -= 20;
+            document.querySelector('#playerHealth').style.width=player.health + '%';
+            console.log("Enemy attack connects")
+    }
+
+    //End game based on health
+    if(enemy.health <= 0 || player.health <= 0) {
+        determineWinner({player, enemy, timerId});
+    }
+}
+animate();
+//-------------------------------Player/Enemy event listeners-------------------------------//
+//When keys are pressed
+window.addEventListener('keydown', (event) =>{
+    switch(event.key) {
+        //Player 1
+        //move right
+        case 'd':
+            keys.d.pressed = true;
+            player.lastKey = 'd';
+            break;
+        
+        //move left
+        case 'a':
+            keys.a.pressed = true;
+            player.lastKey = 'a';
+            break;
+        
+        //move Jump set high
+        case 'w':
+            player.velocity.y = -20;
+            break;
+        
+        //player attacks
+        case ' ':
+            player.attack();
+            break;
+        //End Player 1
+
+        //Enemy
+        //move right
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true;
+            enemy.lastKey = 'ArrowRight';
+            break;
+        
+        //move left
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            enemy.lastKey = 'ArrowLeft';
+            break;
+        
+        //move Jump
+        case 'ArrowUp':
+            enemy.velocity.y = -20;
+            break;
+
+        //Enemy attacks
+        case 'ArrowDown':
+            enemy.attack();
+            break;
+        //End Enemy
+    }
+})
+
+//When key is let go for player
+window.addEventListener('keyup', (event) =>{
+    switch(event.key) {
+        case 'd':
+            keys.d.pressed = false;
+            break;
+
+        case 'a':
+            keys.a.pressed = false;
+            break;
+    }
+
+    //Enemy Keys
+    switch(event.key) {
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;
+            break;
+
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+            break;
+    }
+    console.log(event.key)
+})
+/*TODO: 
+1. have hitboxes turn around to always face player. Check draw() attackbox and offset in code!!
+2. Make sure players cannot double jump
+3. Rename attackbox to hitbox (like if x axis of player is > than enemy or something i dunno)
+4. Add more attacks
+5. Rename Enemy to player 2 or something
+*/
---
+++ JavaScriptPunchy/js/classes.js
@@ -0,0 +1,86 @@
+//Create Sprite
+class Sprite {
+    //Pass these arguements as a group so I don't need to remember the order
+    constructor({position, imageSrc}) {
+        this.position = position;
+        this.width = 50;
+        this.height = 150;
+        this.image = new Image();
+        this.image.src = imageSrc;
+        
+    }
+    //Sprite is drawn when method is called
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+    //Moves sprites around call this method!
+    update() {
+        this.draw();
+    }
+}
+
+//Figters class replaces sprites class
+class Fighter {
+    //Pass these arguements as a group so I don't need to remember the order
+    constructor({position, velocity, color = 'red', offset}) {
+        this.position = position;
+        this.velocity = velocity
+        this.width = 50;
+        this.height = 150;
+        this.lastKey;
+        //hitbox attributes
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            //so hitbox always points towards other player
+            offset,//same syntax as offset: offsets
+            //Where the hitbox will be on the sprite
+            width: 100,
+            height: 50
+        }
+        this.color = color;
+        this.isAttacking;
+        this.health = 100;
+    }
+    //Sprtite is drawn when method is called
+    draw() {
+        //Fill with red color
+        c.fillStyle = this.color;
+        //50px wide and 150px tall at position location
+        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+        //Hitbox is drawn
+        if(this.isAttacking) {
+            c.fillStyle = "green";
+            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
+        }
+    }
+    //Moves sprites around call this method!
+    update() {
+        this.draw();
+        //update hitboxes position in relation of the parent
+        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
+        this.attackBox.position.y = this.position.y;
+        //Player horizontal movement
+        this.position.x += this.velocity.x;
+        //Each frame we loop add "gravity" to drop the sprites
+        this.position.y += this.velocity.y;
+
+
+        //Makes sure that sprite stop dropping past canvas
+        if((this.position.y + this.height + this.velocity.y) >= (canvas.height - 96)) { //Sets where the fighter is placed vertically (change -value for height)
+            this.velocity.y = 0;
+        } 
+        else 
+        //Add gravity if sprites are not past the bottom
+        this.velocity.y += gravity;
+    }
+    //Sets the sprites in an attacking state (isAttacking)
+    attack() {
+        this.isAttacking = true;
+        setTimeout(() => {
+        this.isAttacking = false;
+        }, 100)
+    }
+}
